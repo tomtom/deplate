63c9780d454710b70d667a917190e1c241f4acbf
Tom <micathom@gmail.com>
2010-09-19 19:35:30 +0200

    de.utf8 files

--- lib/deplate/mod/lang-de.utf8.rb
+++ lib/deplate/mod/lang-de.utf8.rb
@@ -5,16 +5,16 @@
 # @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
 # @Created:     07-Mai-2004.
 # @Last Change: 2010-09-19.
-# @Revision:    0.88
+# @Revision:    0.86
 
 require 'deplate/messages'
 
 # German message catalog.
-class Deplate::Messages::DeLatin1 < Deplate::Messages
-    setup 'de'
+class Deplate::Messages::DeUTF8 < Deplate::Messages
+    setup 'de.utf8'
     def_prop 'lang', 'de'
     def_prop 'latex_lang', 'german'
     def_prop 'latex_lang_cmd', %{\\usepackage{german}}
-    load_catalog 'de.latin1'
+    load_catalog 'de.utf8'
 end
 
